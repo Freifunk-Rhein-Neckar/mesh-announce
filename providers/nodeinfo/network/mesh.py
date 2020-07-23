9ffc3e8ca12bfca4ea2fc7df8c494bedc34baa51
herbetom <git@herbetom.de>
2020-07-23 02:00:08 +0200

    use sudo for batctl call this allows running mesh-announce as a non root user

--- providers/nodeinfo/network/mesh.py
+++ providers/nodeinfo/network/mesh.py
@@ -13,7 +13,7 @@ class Source(providers.DataSource):
                         open('/sys/class/net/{}/address'.format(iface)).read().strip()
                         for iface in map(
                             lambda line: line.split(':')[0],
-                            call(['batctl', 'meshif', batadv_dev, 'if'])
+                            call(['sudo', 'batctl', 'meshif', batadv_dev, 'if'])
                         )
                     ]
                 }
