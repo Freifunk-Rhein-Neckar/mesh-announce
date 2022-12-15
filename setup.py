344d2e5eef421b844a65ee34df0f3f8b10410fe8
Tom Herbers <mail@tomherbers.de>
2022-12-15 13:20:26 +0100

    r32

--- setup.py
+++ setup.py
@@ -5,7 +5,10 @@ from setuptools import setup, find_packages
 setup(name='mesh-announce',
 	version='1.0',
 	# Modules to import from other scripts:
-	packages=find_packages(),
+	packages=(find_packages(
+		where='/',
+		include=['pkg*'],  # alternatively: `exclude=['additional*']`
+	)+["announce.py", "config.py", "domain.py", "metasocketserver.py", "util.py"]),
 	# Executables
 	scripts=["announce.py", "respondd.py"],
 )
