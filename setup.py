934809960dbf4d78aeab36c127206de2afcdbcbb
Tom Herbers <mail@tomherbers.de>
2022-12-15 13:29:09 +0100

    r32

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup, find_packages
 setup(name='mesh-announce',
 	version='1.0',
 	# Modules to import from other scripts:
-	packages=(find_packages(),
+	packages=(find_packages()),
 	#)+["announce", "config", "domain", "metasocketserver", "util"]),
 	include_package_data=True,
 	# Executables
